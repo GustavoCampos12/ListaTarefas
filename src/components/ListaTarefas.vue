c0ff6c3c939fc7c8c3b612151335954a701f6bb0
Gustavo Campos <guustavo1619@gmail.com>
2024-06-04 17:54:07 -0300

    Aplicação Modal concluída

--- src/components/ListaTarefas.vue
+++ src/components/ListaTarefas.vue
@@ -1,55 +1,69 @@
 <template>
-    <div class="container">
-      <h1>Lista de Tarefas</h1>
-      <div class="form-todo form-group">
-        <p>
-          <input placeholder="nome" type="text" name="author" class="form-control" v-model="name"/>    
-        </p>
-        <p>
-          <textarea placeholder="Tarefa" name="message" class="form-control" v-model="message"></textarea>
-        </p>
-        <button @click="addTarefa" type="submit" class="btn btn-primary">Adicionar</button>
-        <div class="list-group">
-          <div class="list-group-item" v-for="(tarefa, index) in todasTarefas" :key="index">
-            <span class="comment_author">Nome: <strong>{{ tarefa.name }}</strong></span>
-            <p>Tarefa: {{ tarefa.message }}</p>
-            <div>
-              <a @click.prevent="removeTarefa(index)" href="#" title="Excluir">Excluir</a>
-            </div>
+  <div class="container">
+    <h1>Lista de Tarefas</h1>
+    <div class="form-todo form-group">
+      <p>
+        <input placeholder="nome" type="text" name="author" class="form-control" v-model="name"/>    
+      </p>
+      <p>
+        <textarea placeholder="Tarefa" name="message" class="form-control" v-model="message"></textarea>
+      </p>
+      <button @click="showConfirmModal" type="submit" class="btn btn-primary">Adicionar</button>
+      <div class="list-group">
+        <div class="list-group-item" v-for="(tarefa, index) in todasTarefas" :key="index">
+          <span class="comment_author">Nome: <strong>{{ tarefa.name }}</strong></span>
+          <p>Tarefa: {{ tarefa.message }}</p>
+          <div>
+            <a @click.prevent="removeTarefa(index)" href="#" title="Excluir">Excluir</a>
           </div>
         </div>
-      </div> 
+      </div>
     </div>
-  </template>
-  
-  <script setup>
-    import {ref, computed} from 'vue';
+    <ModalConfirm v-if="showModal" @confirm="addTarefa" @cancel="hideConfirmModal" />
+  </div>
+</template>
 
-    const tarefas = ref([]);
-    const name = ref('');
-    const message = ref('');
+<script setup>
+  import { ref, computed } from 'vue';
+  import ModalConfirm from './ModalConfirm.vue';
 
-    const addTarefa = () =>{
-      if(message.value.trim() ===''){
-        return;
-      }
-      tarefas.value.push({
-        name: name.value,
-        message: message.value
-      });
-      name.value = '';
-      message.value = '';
+  const tarefas = ref([]);
+  const name = ref('');
+  const message = ref('');
+  const showModal = ref(false);
+
+  const addTarefa = () => {
+    if (message.value.trim() === '') {
+      return;
     }
+    tarefas.value.push({
+      name: name.value,
+      message: message.value
+    });
+    name.value = '';
+    message.value = '';
+    hideConfirmModal();
+  }
 
-    const removeTarefa = (index) =>{
-      tarefas.value.splice(index, 1);
+  const removeTarefa = (index) => {
+    tarefas.value.splice(index, 1);
+  }
+
+  const todasTarefas = computed(() => {
+    return tarefas.value.map(tarefa => ({
+      ...tarefa,
+      name: tarefa.name.trim() === '' ? 'Anônimo' : tarefa.name
+    }));
+  })
+
+  const showConfirmModal = () => {
+    if(message.value.trim() === ''){
+      return;
     }
+    showModal.value = true;
+  }
 
-    const todasTarefas = computed(() =>{
-      return tarefas.value.map(tarefa =>({
-        ...tarefa,
-        name: tarefa.name.trim() === ''? 'Anônimo' : tarefa.name
-      }));
-    })
-  </script>
-  
+  const hideConfirmModal = () => {
+    showModal.value = false;
+  }
+</script>
---
+++ src/components/ModalConfirm.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="modal">
+      <div class="modal-content">
+        <p>Deseja mesmo adicionar essa tarefa?</p>
+        <div class="btn-modal">
+        <button class="btn-confirm" @click="confirm">Confirmar</button>
+        <button class="btn-close" @click="cancel">Cancelar</button>
+        </div>
+    </div>
+    </div>
+  </template>
+  
+  <script setup>
+    import { defineEmits } from 'vue';
+  
+
+    const emit = defineEmits(['confirm', 'cancel']);
+  
+    const confirm = () => {
+      emit('confirm');
+    }
+  
+    const cancel = () => {
+      emit('cancel');
+    }
+  </script>
+  
+  <style >
+    .modal {
+      position: fixed;
+      top: 0;
+      left: 0;
+      background-color: rgba(0, 0, 0, 0.5);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  
+    .modal-content {
+      width: 300px;
+      height: 150px;
+      background-color: white;
+      padding: 20px;
+      border-radius: 5px;
+    }
+
+    .btn-close{
+        margin-left: 50px;
+    }
+    .btn-confirm,.btn-close{
+        width: 100px;
+        background-color: #007bff ;
+        border-color: #007bff;
+        color: #fff;
+        border-radius: 5px;
+    }
+  </style>
+  
